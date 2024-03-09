5123294abd7f2eea425105359e124ed42da11d63
burns115 <rburns@email.neit.edu>
2024-03-09 14:06:41 -0500

    Making Changes

---
+++ BlockBusterConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using BlockBuster;
+
+namespace BlockBusterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = BlockBusterBasicFunctions.GetAllMoviesOfGenre();
+            var oh = new OutputHelper();
+            oh.WriteToConsole(b);
+        }
+    }
+}
